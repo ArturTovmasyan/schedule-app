277143d59bab18ec55e5cb69f30fc01c1f7651ab
Artur Tovmasyan <artur.tovmasyan777@gmail.com>
2024-05-19 18:34:15 +0400

    added timezone in calendar header

--- client/src/app/core/components/calendar/calendar.component.ts
+++ client/src/app/core/components/calendar/calendar.component.ts
@@ -10,6 +10,7 @@ export class CalendarComponent implements OnDestroy {
   subscription: BehaviorSubject<boolean>;
   calendarFullSize: boolean = false;
   displayStyle: string = '';
+  timezone = new Date().toString().match(/([A-Z]+[\+-][0-9]+.*)/)?.[1];
 
   constructor(private readonly broadcaster: BroadcasterService) {
     this.subscription = this.broadcaster.on('calendar_full_size').subscribe(() => {
