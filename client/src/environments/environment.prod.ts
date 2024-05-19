9fe8c7767707add0a39591c9d1b7768ca4966f08
Artur Tovmasyan <artur.tovmasyan777@gmail.com>
2024-05-19 15:53:34 +0400

    Add ms configs data into env.prod

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  ms_client_id: '8612bd9c-2d7d-48bb-84a5-55984f126de4',
+  ms_authority: 'https://login.microsoftonline.com/f8cdef31-a31e-4b4a-93e4-5f571e91255a',
+  ms_redirect_url: 'http://localhost:4200/api/auth/microsoft/callback'
 };
