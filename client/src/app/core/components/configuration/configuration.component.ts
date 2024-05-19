6b3ec196b4d13afe0a6df6c46c89706c3d71781a
Artur Tovmasyan <artur.tovmasyan777@gmail.com>
2024-05-19 17:01:40 +0400

    add option to add/remove domains

--- client/src/app/core/components/configuration/configuration.component.ts
+++ client/src/app/core/components/configuration/configuration.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { first } from 'rxjs/operators';
 import { CalendarAccessibility } from '../../interfaces/calendar/accessibility.calendar.inteface';
 import { AccessibilityService } from '../../services/calendar/accessibility.service';
@@ -15,14 +15,20 @@ enum AccessibilityType {
   styleUrls: ['./configuration.component.scss']
 })
 export class ConfigurationComponent implements OnInit {
-
+  @ViewChild('domainInput')
+  domainInput!: ElementRef;
   currentAccessibility: CalendarAccessibility | null = null
+  currentDisplaySection: Section = Section.DOMAIN;
   error: any | null = null;
 
   constructor(private calendarAccessibilityService: AccessibilityService) {}
 
   ngOnInit(): void {
     this.fetchAccessibility();
+  }
+
+  get domains(): string[] {
+    return this.currentAccessibility?.domains ?? [];
   }
 
   fetchAccessibility() {
@@ -81,7 +87,41 @@ export class ConfigurationComponent implements OnInit {
     return false
   }
 
+  toggleSection(section: Section) {
+    this.currentDisplaySection = this.currentDisplaySection == section ? Section.NONE : section;    
+  }
+
+  isVisible(section: Section): boolean {
+    return this.currentDisplaySection == Section.NONE || this.currentDisplaySection == section;
+  }
+
+  addDomain() {
+    const domain = this.domainInput.nativeElement.value.replace('@', '');
+    if((this.currentAccessibility?.domains ?? []).indexOf(domain) < 0) {
+      const domains = [...this.currentAccessibility?.domains ?? [], domain];
+      this.updateAccessibility(AccessibilityType.DOMAIN, true, domains);
+    }
+    this.domainInput.nativeElement.value = "";
+  }
+
+  removeDomain(index: number) {
+    const domains = [...this.currentAccessibility?.domains ?? []];
+    domains.splice(index, 1);
+    this.updateAccessibility(AccessibilityType.DOMAIN, true, domains);
+  }
+
   get AccessibilityType(): typeof AccessibilityType {
     return AccessibilityType;
   }
+
+  get Section(): typeof Section {
+    return Section;
+  }
+}
+
+enum Section {
+  NONE,
+  PUBLIC,
+  DOMAIN,
+  REQUEST
 }
