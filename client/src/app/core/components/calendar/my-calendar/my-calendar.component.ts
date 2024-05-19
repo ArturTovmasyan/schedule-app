36b35b39dd34fdb11ed202daae0812fe84fff9a4
Artur Tovmasyan <artur.tovmasyan777@gmail.com>
2024-05-19 17:10:58 +0400

    Add style for event block

--- client/src/app/core/components/calendar/my-calendar/my-calendar.component.ts
+++ client/src/app/core/components/calendar/my-calendar/my-calendar.component.ts
@@ -3,6 +3,7 @@ import {CalendarOptions} from "@fullcalendar/angular";
 import {first} from "rxjs/operators";
 import {CalendarService} from "../../../services/calendar/calendar.service";
 import DateConverter from "../../helpers/date.converter";
+// const $ = require( "jquery" )
 
 @Component({
   selector: 'app-my-calendar',
@@ -14,15 +15,13 @@ export class MyCalendarComponent implements OnInit {
   error: any = null;
   calendarApi: any;
 
-  constructor(private calendarService: CalendarService) {
-  }
+  constructor(private calendarService: CalendarService) {}
 
   ngOnInit(): void {
     // this.fetchMyEvents();
   }
 
   fetchMyEvents() {
-
     this.calendarService.fetchEvents()
       .pipe(first())
       .subscribe({
@@ -30,7 +29,6 @@ export class MyCalendarComponent implements OnInit {
           const data: any[] = [];
           if (events.length > 0) {
             events.forEach((el: any) => {
-              debugger;
               data.push({
                 start: DateConverter.convertUTCDateToLocalDate(el.start),
                 end: DateConverter.convertUTCDateToLocalDate(el.end),
@@ -38,7 +36,6 @@ export class MyCalendarComponent implements OnInit {
                 description: el.description
               })
             })
-
             this.calendarOptions.events = data;
           }
         },
@@ -56,18 +53,17 @@ export class MyCalendarComponent implements OnInit {
     selectable: true,
     allDaySlot: false,
     droppable: true,
-    eventContent: this.eventContent.bind(this),
-    dateClick: this.handleDateClick.bind(this),
     eventTextColor: 'black',
     eventBackgroundColor: '#E9F6FD',
     eventOrderStrict: true,
+    stickyHeaderDates: true,
+    titleFormat: {
+      year: 'numeric', month: 'long', day: 'numeric'
+    },
     eventTimeFormat: {
       hour: 'numeric',
       minute: '2-digit',
       meridiem: 'short'
-    },
-    titleFormat: {
-      year: 'numeric', month: 'long', day: 'numeric'
     },
     headerToolbar: {
       left: '',
@@ -75,11 +71,29 @@ export class MyCalendarComponent implements OnInit {
       right: 'today'
     },
     events: [
-      {title: 'event 1', start: '2022-10-03T03:45:00', end: '2022-10-03T04:45:00', description: 'Event 1 description'},
-      {title: 'event 2', start: '2022-10-04T07:45:00', end: '2022-10-04T08:45:00', description: 'Event 2 description'},
-      {title: 'event 3', start: '2022-10-05T02:45:00', end: '2022-10-05T05:45:00', description: 'Event 3 description'},
-      {title: 'event 4', start: '2022-10-06T10:45:00', end: '2022-10-06T12:45:00', description: 'Event 4 description'},
+      {title: 'event 1 Test big title for resize event block', start: '2022-10-04T03:45:00', end: '2022-10-04T04:45:00', description: 'Event 1 description'},
+      {title: 'event 2', start: '2022-10-05T07:45:00', end: '2022-10-05T08:45:00', description: 'Event 2 description'},
+      {title: 'event 3', start: '2022-10-06T02:45:00', end: '2022-10-06T05:45:00', description: 'Event 3 description'},
+      {title: 'event 4', start: '2022-10-07T10:45:00', end: '2022-10-07T12:45:00', description: 'Event 4 description'},
     ],
+    eventContent: this.eventContent.bind(this),
+    eventClassNames: function(arg) {
+      const title = arg.event._def.title;
+      if (title.length > 20) {
+        return 'event-big';
+      } else {
+        return 'event-standard';
+      }
+    },
+    // eventClick: function(arg) {
+    //   if (confirm('Are you sure you want to delete this event?')) {
+    //     arg.event.remove()
+    //   }
+    // },
+    // dateClick: this.handleDateClick.bind(this),
+    // select: function(arg) {
+    //   debugger;
+    // },
   };
 
   eventContent(arg: any) {
@@ -87,21 +101,11 @@ export class MyCalendarComponent implements OnInit {
     // let description = arg.event._def.extendedProps['description'];
     let title = arg.event.title;
     let time = arg.timeText;
-
-    divEl.style.height = '200px';
-    divEl.style.borderLeft = '10px solid #4AA3E1';
-    divEl.innerHTML = '<strong>' + title + '</strong><br/><span style="color: #4AA3E1">' + time + '</span>';//<br/><span>'+description+'</span>'
-
-    // if (description.length > 6) {
-    // 10px solid #4AA3E1
-    //   $('div.fc-timegrid-event-harness').css('width', '150');
-    // }
-
-    // let arrayOfDomNodes = [divEl];
+    divEl.innerHTML = '<span>' + title + '</span><br/><span style="color: #047BDAFF">' + time + '</span>';
     return {html: divEl.innerHTML};
   }
 
   handleDateClick(arg: { dateStr: string; }) {
-    // alert('date click! ' + arg.dateStr)
+    alert('date click! ' + arg.dateStr)
   }
 }
