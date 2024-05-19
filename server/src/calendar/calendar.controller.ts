b50c85876043068470a80677dc183f8a5ed914b2
Artur Tovmasyan <artur.tovmasyan777@gmail.com>
2024-05-19 16:24:08 +0400

    fix comments and hard codes

--- server/src/calendar/calendar.controller.ts
+++ server/src/calendar/calendar.controller.ts
@@ -23,42 +23,6 @@ import UpdateEventDto from './dto/updateEvent.dto';
 export class CalendarController {
   constructor(private readonly calendarService: CalendarService) {}
 
-  //TODO remove this comments
-
-  // @Get('google')
-  // @UseGuards(AuthGuard())
-  // @UseInterceptors(UpdateAccessTokenInterceptor)
-  // async getUserGoogleCalendar(@Req() req: { user: User }) {
-  //   // req.user = { ...req.user, id: '947344d9-7a3b-416d-b17d-bf6626988c16' };
-  //   // req.user = { ...req.user, id: '526bcc4e-f73d-4c25-95e5-7f41053355e8' };
-  //   return await this.calendarService.getCalendarsFromGoogle(req.user);
-  // }
-  //
-  // @Get('google-events')
-  // @UseGuards(AuthGuard())
-  // @UseInterceptors(UpdateAccessTokenInterceptor)
-  // async getUserGoogleCalendarEvents(@Req() req: { user: User }) {
-  //   // req.user = { ...req.user, id: '947344d9-7a3b-416d-b17d-bf6626988c16' };
-  //   // req.user = { ...req.user, id: '526bcc4e-f73d-4c25-95e5-7f41053355e8' };
-  //   return await this.calendarService.syncGoogleCalendarEventList(req.user);
-  // }
-  //
-  // @Get('ms-events')
-  // @UseGuards(AuthGuard())
-  // @UseInterceptors(UpdateAccessTokenInterceptor)
-  // async getUserOutlookCalendarEvents(@Req() req: { user: User }) {
-  //   // req.user = { ...req.user, id: '947344d9-7a3b-416d-b17d-bf6626988c16' };
-  //   // req.user = { ...req.user, id: '526bcc4e-f73d-4c25-95e5-7f41053355e8' };
-  //   return await this.calendarService.syncOutlookCalendarEventList(req.user);
-  // }
-  //
-  // @Get('ms')
-  // @UseGuards(AuthGuard())
-  // @UseInterceptors(UpdateAccessTokenInterceptor)
-  // async getUserMSCalendar(@Req() req: { user: User }) {
-  //   return await this.calendarService.getCalendarsFromOutlook(req.user);
-  // }
-
   @Get('events')
   @UseGuards(AuthGuard())
   @UseInterceptors(UpdateAccessTokenInterceptor)
