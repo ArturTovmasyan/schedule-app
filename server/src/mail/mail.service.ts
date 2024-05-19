c2ea2f9888c3962194085b3daa9520d1a18bf11d
Artur Tovmasyan <artur.tovmasyan777@gmail.com>
2024-05-19 16:01:35 +0400

    replace mailgun with sendgrid

--- server/src/mail/mail.service.ts
+++ server/src/mail/mail.service.ts
@@ -1,27 +1,21 @@
 import { Injectable } from '@nestjs/common';
-import * as Mailgun from 'mailgun-js';
-import { IMailGunData } from './interfaces/mail.interface';
-import {ConfigService} from "@nestjs/config";
+import { ConfigService } from "@nestjs/config";
+import * as SendGrid from '@sendgrid/mail';
 
 @Injectable()
 export class MailService { // TODO use (queue AMQP for send)
-    private mg: Mailgun.Mailgun;
 
     constructor(readonly configService: ConfigService) {
-        this.mg = Mailgun({
-            apiKey: configService.get<string>('MAILGUN_API_KEY'),
-            domain: configService.get<string>('MAILGUN_API_DOMAIN')
-        });
+        SendGrid.setApiKey(configService.get<string>('SENDGRID_API_KEY'));
     }
 
-    send(data: IMailGunData): Promise<Mailgun.messages.SendResponse> {
-        return new Promise((res, rej) => {
-            this.mg.messages().send(data, function(error, body) {
-                if (error) {
-                    rej(error);
-                }
-                res(body);
-            });
+    send(mail: SendGrid.MailDataRequired): Promise<[SendGrid.ClientResponse, {}]> {
+        return new Promise((resolve, reject) => {
+            SendGrid.send(mail)
+                .then((response) => resolve(response))
+                .catch((error) => {
+                    reject(error)
+                });
         });
     }
 }
