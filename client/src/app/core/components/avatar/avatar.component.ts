67ca57b475c9cec29b2ca332431b4a7a38c90791
Artur Tovmasyan <artur.tovmasyan777@gmail.com>
2024-05-19 18:58:41 +0400

    on click of avatar in sharable link left bar bug two '/api/auth/logged' API call on every dashboard  page load solved

--- client/src/app/core/components/avatar/avatar.component.ts
+++ client/src/app/core/components/avatar/avatar.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
 import {AuthService} from "../../services/auth/auth.service";
 
 @Component({
@@ -6,21 +7,34 @@ import {AuthService} from "../../services/auth/auth.service";
   templateUrl: './avatar.component.html',
   styleUrls: ['./avatar.component.scss']
 })
-export class AvatarComponent implements OnInit {
+export class AvatarComponent implements OnInit, OnDestroy {
 
-  fullName:string = '';
-  avatar:string = '';
+  fullName = '';
+  avatar = '';
+  subscription$!: Subscription;
+
+  @Input() showDropdown = true;
 
   constructor(private authService:AuthService) { }
 
   ngOnInit(): void {
-    this.authService.hasAccess().subscribe({
-      next: ({ user }) => {
-        if (user) {
-          this.fullName = user.firstName+ ' ' + user.lastName;
-          this.avatar = user.avatar;
-        }
-      }
+    this.subscription$ = this.authService.currentUser.subscribe(res => {
+      this.fullName = res?.user?.fullName ?? '';
+      this.avatar = res?.user?.avatar ?? '';
     });
+    // removed as hasAccess is called from gaurd also
+    // this.authService.hasAccess().subscribe({
+    //   next: ({ user }) => {
+    //     if (user) {
+    //       this.fullName = user.firstName+ ' ' + user.lastName;
+    //       this.avatar = user.avatar;
+    //     }
+    //   }
+    // });
+  }
+
+  ngOnDestroy(): void {
+    this.subscription$.unsubscribe();
+
   }
 }
